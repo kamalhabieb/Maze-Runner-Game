10d285f1fcd5c677f6f84c2073389e7084be1f11
kamal <kamalhabieb@yahoo.ca>
2017-12-08 14:57:50 +0200

    adding Engine interface.

---
+++ src/main/java/models/engine/Engine.java
@@ -0,0 +1,35 @@
+package models.engine;
+
+public interface Engine {
+
+
+    /**
+     * Gets the object's x coordinate
+     *
+     * @return the x coordinate
+     */
+    int getX();
+
+    /**
+     * Sets the object's x coordinate
+     *
+     * @param x coordinate
+     */
+    void setX(int x);
+
+    /**
+     * Gets the object's x coordinate
+     *
+     * @return the y coordinate
+     */
+
+    int getY();
+
+    /**
+     * Sets the object's y coordinate
+     *
+     * @param y coordinate
+     */
+    void setY(int y);
+
+}
