4034a600c346535a835f432f241b58563e145705
karim-mohamed <k.mohamed3@hotmail.com>
2017-12-08 02:53:58 +0200

    adding wall interface.

--- src/main/java/models/wall/wall.java
+++ src/main/java/models/wall/wall.java
@@ -3,18 +3,17 @@ package models.wall;
 public interface wall {
 
     /**
-     * Get wither the cell is breakable or not  .
-     *
-     * @return makeBreakable if true then the cell is breakable , else it is unbreakable .
-     */
-    boolean getCellState();
-
-    /**
      * make the cell either breakable or not .
-     *
      * @param makeBreakable if true then make the cell breakable , else make it unbreakable .
      */
     void setCellState(boolean makeBreakable);
+
+    /**
+     * Get wither the cell is breakable or not .
+     *
+     * @return true if the cell is breakable or false if it is unbreakable .
+     */
+    boolean getCellState();
 
     /**
      * Break the cell if breakable and put a bomb or empty space .
