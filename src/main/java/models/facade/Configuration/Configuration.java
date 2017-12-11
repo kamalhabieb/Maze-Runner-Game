22d3b7447d9299d1c73a0c4b3250fc48c9259d0d
karim-mohamed <k.mohamed3@hotmail.com>
2017-12-11 18:39:57 +0200

    implementing configuration

--- src/main/java/models/facade/Configuration/Configuration.java
+++ src/main/java/models/facade/Configuration/Configuration.java
@@ -2,19 +2,23 @@ package models.facade.Configuration;
 
 import models.maze.MazeBuilder;
 import models.mazeObjects.bomb.Bomb;
+import models.mazeObjects.gift.ObjectsFactory;
 
 import java.awt.*;
+import java.util.HashSet;
 import java.util.Properties;
 import java.util.Random;
 
 public  class Configuration {
 
     private Properties info;
+
     private int bombsNum;
     private int giftsNum;
     private int monstersNum;
     private String gameMode;
     private int difficulty;
+    private HashSet listOfWallPositions;
 
     private Random rand= new Random();
     public Configuration(Properties info)
@@ -24,18 +28,43 @@ public  class Configuration {
         this.monstersNum=Integer.parseInt(info.getProperty("number_of_monsters"));
         this.gameMode=info.getProperty("game_mode");
         this.difficulty=Integer.parseInt(info.getProperty("difficulty"));
+        this.listOfWallPositions= this.setPositionsSet(info.getProperty("tempWallsList"));
     }
 
     public void loadConfiguration(){
         MazeBuilder builder = new MazeBuilder();
+        Object bomb;
 
         for (int i=0;i<bombsNum;i++)
         {
             int x=rand.nextInt(30);
             int y=rand.nextInt(30);
+            Point bombPosition = new Point(x,y);
             int range=rand.nextInt(5)+1;
-            //Object bomb =;
+
+            if (!listOfWallPositions.contains(bombPosition))
+                bomb =ObjectsFactory.produce(ObjectsFactory.BOMB,range,bombPosition);
         }
+    }
+
+
+    private HashSet setPositionsSet (String WallPositions)
+    {
+        HashSet<Point> setOfWallCells = new HashSet();
+        String[] pointString = WallPositions.split(" ");
+        for (String s : pointString) {
+            s.replace("(", "");
+            s.replace(")", "");
+
+            int x = Integer.parseInt(s.split(",")[0]);
+            int y = Integer.parseInt(s.split(",")[1]);
+
+            Point wallCellPoint = new Point(x, y);
+
+            setOfWallCells.add(wallCellPoint);
+        }
+
+        return setOfWallCells;
     }
 
 }
