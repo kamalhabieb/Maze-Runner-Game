0e1b62ede6fe5c7bd5069766219e1dc2e17d36e8
karim-mohamed <k.mohamed3@hotmail.com>
2017-12-11 17:32:22 +0200

    implementing wall

--- src/main/java/models/facade/Configuration/Configuration.java
+++ src/main/java/models/facade/Configuration/Configuration.java
@@ -1,21 +1,41 @@
 package models.facade.Configuration;
 
 import models.maze.MazeBuilder;
+import models.mazeObjects.bomb.Bomb;
 
+import java.awt.*;
 import java.util.Properties;
+import java.util.Random;
 
 public  class Configuration {
 
     private Properties info;
+    private int bombsNum;
+    private int giftsNum;
+    private int monstersNum;
+    private String gameMode;
+    private int difficulty;
 
-    public Configuration(Properties info) {
-        this.info = info;
+    private Random rand= new Random();
+    public Configuration(Properties info)
+    {
+        this.bombsNum= Integer.parseInt(info.getProperty("number_of_bombs"));
+        this.giftsNum=Integer.parseInt(info.getProperty("number_of_gifts"));
+        this.monstersNum=Integer.parseInt(info.getProperty("number_of_monsters"));
+        this.gameMode=info.getProperty("game_mode");
+        this.difficulty=Integer.parseInt(info.getProperty("difficulty"));
     }
-
 
     public void loadConfiguration(){
         MazeBuilder builder = new MazeBuilder();
 
+        for (int i=0;i<bombsNum;i++)
+        {
+            int x=rand.nextInt(30);
+            int y=rand.nextInt(30);
+            int range=rand.nextInt(5)+1;
+            Bomb bomb = new Bomb(range,new Point(x,y));
+        }
     }
 
 }
