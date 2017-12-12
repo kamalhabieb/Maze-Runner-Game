1c7bea596aec2368c669d524c7d8c674ced74359
karim-mohamed <k.mohamed3@hotmail.com>
2017-12-12 10:23:17 +0200

    implementing configuration

--- src/main/java/models/facade/Configuration/Configuration.java
+++ src/main/java/models/facade/Configuration/Configuration.java
@@ -1,8 +1,8 @@
 package models.facade.Configuration;
 
 import models.maze.MazeBuilder;
+import models.maze.MazeObject;
 import models.mazeObjects.ObjectsFactory;
-import models.mazeObjects.bomb.Bomb;
 
 import java.awt.*;
 import java.util.HashSet;
@@ -11,16 +11,14 @@ import java.util.Random;
 
 public  class Configuration {
 
-    private Properties info;
-
     private int bombsNum;
     private int giftsNum;
     private int monstersNum;
     private String gameMode;
     private int difficulty;
-    private HashSet listOfWallPositions;
-
+    private HashSet <String> listOfWallPositions;
     private Random rand= new Random();
+
     public Configuration(Properties info)
     {
         this.bombsNum= Integer.parseInt(info.getProperty("number_of_bombs"));
@@ -33,35 +31,39 @@ public  class Configuration {
 
     public void loadConfiguration(){
         MazeBuilder builder = new MazeBuilder();
-        Object bomb;
+
 
         for (int i=0;i<bombsNum;i++)
         {
-            int x=rand.nextInt(30);
-            int y=rand.nextInt(30);
+            int x=rand.nextInt(30);//replace 30 with maze Width of the template
+            int y=rand.nextInt(30);//replace 30 with maze Length of the template
             Point bombPosition = new Point(x,y);
+            String bombPositionString = bombPosition.toString();
             int range=rand.nextInt(5)+1;
 
-            if (!listOfWallPositions.contains(bombPosition))
-                bomb = ObjectsFactory.produce(ObjectsFactory.BOMB,range,bombPosition);
+            if ( !listOfWallPositions.contains(bombPositionString) )
+            {
+                MazeObject bomb = ObjectsFactory.produce(ObjectsFactory.BOMB, range, bombPosition);
+                builder.addMazeObject(bomb,bombPosition);
+            }
         }
     }
 
 
     private HashSet setPositionsSet (String WallPositions)
     {
-        HashSet<Point> setOfWallCells = new HashSet();
+        HashSet<String> setOfWallCells = new HashSet();
         String[] pointString = WallPositions.split(" ");
         for (String s : pointString) {
-            s.replace("(", "");
-            s.replace(")", "");
+            s=s.replace("(", "");
+            s=s.replace(")", "");
 
             int x = Integer.parseInt(s.split(",")[0]);
             int y = Integer.parseInt(s.split(",")[1]);
 
             Point wallCellPoint = new Point(x, y);
-
-            setOfWallCells.add(wallCellPoint);
+            String wallCellPointString = wallCellPoint.toString();
+            setOfWallCells.add(wallCellPointString);
         }
 
         return setOfWallCells;
