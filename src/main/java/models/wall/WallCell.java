b62740cadf681a78ca47590300b92295fdc68b32
karim-mohamed <k.mohamed3@hotmail.com>
2017-12-15 19:50:46 +0200

    wallImage

--- src/main/java/models/wall/WallCell.java
+++ src/main/java/models/wall/WallCell.java
@@ -1,30 +1,33 @@
 package models.wall;
+import javafx.scene.image.Image;
+import views.Drawable;
+import views.flyweight.WallImage;
 
-public class WallCell implements Wall {
+public class WallCell extends Drawable implements Wall  {
 
     private boolean breakable ;
     private int MAX_HEALTH=100;
+    private int THREE_QUARTERS_HEALTH=75;
+    private int HALF_HEALTH=50;
+    private int QUARTER_HEALTH=25;
     private int MIN_HEALTH=0;
     private int health ;
 
     public WallCell()
     {
-
-
-
         health = MAX_HEALTH;
     }
 
 
     @Override
-    public void setCellState(boolean makeBreakable) {
-        if(makeBreakable)
-            breakable=true;
-        else breakable=false;
+    public void setBreakable(boolean breakable) {
+        if(breakable)
+            this.breakable=true;
+        else this.breakable=false;
     }
 
     @Override
-    public boolean getCellState() {
+    public boolean isBreakable() {
         if (breakable)
             return true;
         return false;
@@ -44,5 +47,29 @@ public class WallCell implements Wall {
             return true;
         }
         return false;
+    }
+
+    @Override
+    public Image getImage() {
+
+        if (this.isBreakable())
+        {
+            int health = this.getHealth();
+
+            if (health<=THREE_QUARTERS_HEALTH)
+            {
+                return WallImage.getImage(WallImage.breakingState1);
+            }else if (health<=HALF_HEALTH)
+            {
+                return WallImage.getImage(WallImage.breakingState2);
+            }else if (health<=QUARTER_HEALTH)
+            {
+                return WallImage.getImage(WallImage.breakingState3);
+            }
+
+            return WallImage.getImage(WallImage.breakable);
+        }
+
+        return WallImage.getImage(WallImage.unBreakable);
     }
 }
