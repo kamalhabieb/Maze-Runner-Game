c6a818dbd2f90315da1cafb1fa0db34d750a1f44
akrammousa <akrammousa2014@gmail.com>
2017-12-15 18:17:58 +0200

    Maze.

--- src/main/java/models/facade/Facade.java
+++ src/main/java/models/facade/Facade.java
@@ -1,5 +1,8 @@
 package models.facade;
 
+import models.charcter.Player;
+import models.engine.Engine;
+import models.engine.EngineFactory;
 import models.facade.Configuration.Configuration;
 import models.maze.GameMaze;
 import models.maze.Maze;
@@ -9,6 +12,8 @@ import java.util.Properties;
 
 public class Facade implements ControlTower,Observer {
     private Maze mazeG ;
+    private Engine gameEngine;
+    private Player player;
     public final String EASY="/easy.configuration";
     public final String MEDIUM="/medium.configuration";
     public final String HARD="/easy.configuration";
@@ -28,5 +33,9 @@ public class Facade implements ControlTower,Observer {
         }
         Configuration configuration = new Configuration(gameInfo);
         mazeG = configuration.loadConfiguration();
+       gameEngine = EngineFactory.getInstance(gameInfo.getProperty("game_mode"));
+    }
+    public void doAction(){
+
     }
 }
