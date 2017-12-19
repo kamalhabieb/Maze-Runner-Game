10a08e365be5cdb795376e1ad8a75fa62c10221c
Mario Hunter <mario.hunter.std@alexu.edu.eg>
2017-12-19 18:11:44 +0200

    Smoothing the camera

--- src/main/java/controllers/mainPlayController.java
+++ src/main/java/controllers/mainPlayController.java
@@ -1,6 +1,8 @@
 package controllers;
 
 import controllers.command.CommandFactory;
+import javafx.animation.Interpolator;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
@@ -11,6 +13,7 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import models.charcter.states.MoveEast;
 import models.facade.DrawObserver;
 import models.facade.Facade;
@@ -27,7 +30,7 @@ import static controllers.command.CommandFactory.commands.*;
 
 public class mainPlayController implements Initializable, DrawObserver {
 
-    private static final int CAMERA_MOVE = 100;
+    private static final int CAMERA_MOVE = 2;
     @FXML
     private Canvas canvas;
     @FXML
@@ -50,11 +53,10 @@ public class mainPlayController implements Initializable, DrawObserver {
         staticCanvas.setHeight(31 * 30);
 
 
-
         facade = new Facade();
         facade.registerObserver(this);
         facade.initializeGame(Facade.EASY);
-
+        translateCamera();
 
        /* facade.populateDrawables();
         facade.notifyDraw();*/
@@ -104,49 +106,57 @@ public class mainPlayController implements Initializable, DrawObserver {
 
     public void onKeyPressed(KeyEvent keyEvent) {
 
-        if (keyEvent.getCode()==KeyCode.SPACE)
-        {
+        if (keyEvent.getCode() == KeyCode.SPACE) {
             facade.excute(CommandFactory.getCommand(shootABullet));
             facade.fireWeapon();
-        }
-        else if(keyEvent.getCode() == KeyCode.RIGHT) {
+        } else if (keyEvent.getCode() == KeyCode.RIGHT) {
             facade.excute(CommandFactory.getCommand(moveEast));
-        }
-        else if (keyEvent.getCode() == KeyCode.LEFT) {
+            translateCamera();
+        } else if (keyEvent.getCode() == KeyCode.LEFT) {
             facade.excute(CommandFactory.getCommand(moveWest));
-        }
-        else if (keyEvent.getCode() == KeyCode.UP) {
+            translateCamera();
+
+        } else if (keyEvent.getCode() == KeyCode.UP) {
             facade.excute(CommandFactory.getCommand(moveNorth));
-        }
-        else if (keyEvent.getCode() == KeyCode.DOWN) {
+            translateCamera();
+
+        } else if (keyEvent.getCode() == KeyCode.DOWN) {
             facade.excute(CommandFactory.getCommand(moveSouth));
+            translateCamera();
+
         }
         if (keyEvent.getCode() == KeyCode.P) {
             GameGUI.camera.setTranslateZ(GameGUI.camera.getTranslateZ() + CAMERA_MOVE);
-        }
-        else if (keyEvent.getCode() == KeyCode.MINUS) {
+        } else if (keyEvent.getCode() == KeyCode.MINUS) {
             GameGUI.camera.setTranslateZ(GameGUI.camera.getTranslateZ() - CAMERA_MOVE);
-        }
-        else if (keyEvent.getCode() == KeyCode.W) {
-            if(!(GameGUI.camera.getTranslateY() - CAMERA_MOVE < 900/2)) {
+        } else if (keyEvent.getCode() == KeyCode.W) {
+            if (!(GameGUI.camera.getTranslateY() - CAMERA_MOVE < 900 / 2)) {
                 GameGUI.camera.setTranslateY(GameGUI.camera.getTranslateY() - CAMERA_MOVE);
             }
-        }
-        else if (keyEvent.getCode() == KeyCode.S) {
+        } else if (keyEvent.getCode() == KeyCode.S) {
             GameGUI.camera.setTranslateY(GameGUI.camera.getTranslateY() + CAMERA_MOVE);
-        }
-        else if (keyEvent.getCode() == KeyCode.A) {
-            if(!(GameGUI.camera.getTranslateX() - CAMERA_MOVE < 1366/2)) {
+        } else if (keyEvent.getCode() == KeyCode.A) {
+            if (!(GameGUI.camera.getTranslateX() - CAMERA_MOVE < 1366 / 2)) {
                 GameGUI.camera.setTranslateX(GameGUI.camera.getTranslateX() - CAMERA_MOVE);
             }
-        }
-        else if (keyEvent.getCode() == KeyCode.D) {
+        } else if (keyEvent.getCode() == KeyCode.D) {
             GameGUI.camera.setTranslateX(GameGUI.camera.getTranslateX() + CAMERA_MOVE);
         }
-        else {
-            GameGUI.camera.setTranslateX(facade.player.getDestinationX()+ 1366 / 2);
-            GameGUI.camera.setTranslateY(facade.player.getDestinationY() + 900 / 2);
-        }
+
+
+    }
+
+    private void translateCamera() {
+        TranslateTransition t = new TranslateTransition(Duration.millis(100), GameGUI.camera);
+        //t.setFromX(GameGUI.camera.getTranslateX());
+        t.setToX(facade.player.getDestinationX() + 1366 / 2);
+        //t.setFromY(GameGUI.camera.getTranslateY());
+        t.setToY(facade.player.getDestinationY() + 900 / 2);
+
+        t.setInterpolator(Interpolator.EASE_BOTH);
+        t.setDelay(Duration.ZERO);
+        t.play();
+
 
     }
 
