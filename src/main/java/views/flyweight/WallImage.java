89cd29a34bd1cc78213e4bb4bc985bcf0fadd761
karim-mohamed <k.mohamed3@hotmail.com>
2017-12-20 22:35:52 +0200

    Merge branch 'master' of https://github.com/Mario-Hunter/maze_runner
    # Conflicts:
    #	src/main/java/models/facade/Facade.java

--- src/main/java/views/flyweight/WallImage.java
+++ src/main/java/views/flyweight/WallImage.java
@@ -6,18 +6,18 @@ public class WallImage {
 
     public final static String unBreakable = "/images/RegularMode/mazeObjects/mainwall.png";
     public final static String breakable = "/images/RegularMode/mazeObjects/breakable.png"; //wall health [76,100]
-    public final static String breakingState1 = "/RegularMode/mazeObjects/.png";//wall health [51,75]
-    public final static String breakingState2 = "/RegularMode/mazeObjects/.png";//wall health [26,50]
+    public final static String breakingState1 = "/RegularMode/mazeObjects/breakable1.png";//wall health [51,75]
+    public final static String breakingState2 = "/RegularMode/mazeObjects/breakable3.png";//wall health [26,50]
 
     private static final Image unBreakableImage;
     private static final Image breakableImage;
-//    private static final Image breakingState1Image;
-//    private static final Image breakingState2Image;
+    private static final Image breakingState1Image;
+    private static final Image breakingState2Image;
     static {
         unBreakableImage = new Image(PlayerImage.class.getResourceAsStream(unBreakable));
         breakableImage = new Image(PlayerImage.class.getResourceAsStream(breakable));
-//        breakingState1Image = new Image(PlayerImage.class.getResourceAsStream(breakingState1));
-//        breakingState2Image = new Image(PlayerImage.class.getResourceAsStream(breakingState2));
+        breakingState1Image = new Image(PlayerImage.class.getResourceAsStream(breakingState1));
+        breakingState2Image = new Image(PlayerImage.class.getResourceAsStream(breakingState2));
     }
     public static Image getImage(String state) {
         switch (state) {
@@ -25,10 +25,10 @@ public class WallImage {
                 return unBreakableImage;
             case breakable:
                 return breakableImage;
-//            case breakingState1:
-//                return breakingState1Image;
-//            case breakingState2:
-//                return breakingState2Image;
+            case breakingState1:
+                return breakingState1Image;
+            case breakingState2:
+                return breakingState2Image;
             default:
                 return unBreakableImage;
         }
